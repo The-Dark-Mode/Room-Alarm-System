d532b001654e1115225f597853ce2113aef8ae5d
The-Dark-Mode <167715404+The-Dark-Mode@users.noreply.github.com>
2024-11-29 14:22:19 +1300

    Update startup.sh

--- v2/startup.sh
+++ v2/startup.sh
@@ -4,5 +4,5 @@
 tmux new-session -d -s motion_detection
 
 # Send commands to the 'motion_detection' session
-tmux send-keys -t motion_detection 'cd ~/Documents/Room-Defense-System/' C-m
+tmux send-keys -t motion_detection 'cd ~/v2/files/' C-m
 tmux send-keys -t motion_detection 'sudo node server.js' C-m
